ebab2bbd4e1783aa8049d7a36e8acf442149ed6a
Jose Ivan Suazo <joshepw@gmail.com>
2012-05-08 17:50:19 -0600

    Inicializando el Proyecto

---
+++ src/proyectoii/ProyectoII.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoii;
+
+/**
+ *
+ * @author JoXhe
+ */
+public class ProyectoII {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/proyectoii/ruleta.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoii;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author JoXhe
+ */
+public class ruleta {
+    ArrayList <String> turno = new turno<String>;
+    
+}
